9d6893fd09a064605dec60b16bf80a8bbf9b3a27
road::runner <github@contrexx.de>
2026-07-01 11:33:58 +0200

    bugfixing

--- components/truma_inetbox/TrumaiNetBoxApp.cpp
+++ components/truma_inetbox/TrumaiNetBoxApp.cpp
@@ -221,6 +221,7 @@ const uint8_t *TrumaiNetBoxApp::lin_multiframe_recieved(const uint8_t *message, 
   } else if (header->message_type == STATUS_FRAME_AIRCON_MANUAL &&
              header->message_length == sizeof(StatusFrameAirconManual)) {
     ESP_LOGI(TAG, "StatusFrameAirconManual");
+    const auto *stat = &statusFrame->airconManual;
     const uint8_t *p = reinterpret_cast<const uint8_t *>(stat);
 
     ESP_LOGI(TAG,
