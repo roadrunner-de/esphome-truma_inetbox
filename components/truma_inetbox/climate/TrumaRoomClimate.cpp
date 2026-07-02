1a4e5fd8f989644bff9611ca014e91652f41719e
road::runner <github@contrexx.de>
2026-07-02 18:23:34 +0200

    caption for heat/cool-mode added

--- components/truma_inetbox/climate/TrumaRoomClimate.cpp
+++ components/truma_inetbox/climate/TrumaRoomClimate.cpp
@@ -71,6 +71,11 @@ void TrumaRoomClimate::control(const climate::ClimateCall &call) {
 
   if (call.get_fan_mode().has_value()) {
     auto fan_mode = *call.get_fan_mode();
+    if (this->mode == climate::CLIMATE_MODE_HEAT_COOL) {
+      this->fan_mode = climate::CLIMATE_FAN_AUTO;
+      this->publish_state();
+      return;
+    }
     auto status_heater = this->parent_->get_heater()->get_status();
     float temp = temp_code_to_decimal(status_heater->target_temp_room, 0);
     if (call.get_target_temperature().has_value()) {
